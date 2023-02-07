5ac8fabcf12ba75db2117dc5dd77ad10b7d83353
rahul <rahulpatidar0191@gmail.com>
2023-02-07 17:38:34 -0600

    add work-dir

--- scripts/DockerDown.sh
+++ scripts/DockerDown.sh
@@ -2,10 +2,16 @@
 APP_NAME=$1
 REGISTRY=$2
 CLEAN_BRANCH_NAME=$3
-
+WORK_DIR=$4
 # Set registry and clean branch name as global variables
 export REGISTRY=$REGISTRY
 export CLEAN_BRANCH_NAME=$CLEAN_BRANCH_NAME 
+
+if [[ -n $WORK_DIR ]] 
+then
+    echo "WORK_DIR ${WORK_DIR}"
+    cd $WORK_DIR
+fi  
 
 echo "Docker down"
 docker-compose -f docker-compose.yml -f docker-compose.pipeline.yml down
--- scripts/DockerUp.sh
+++ scripts/DockerUp.sh
@@ -2,11 +2,17 @@
 APP_NAME=$1
 REGISTRY=$2
 CLEAN_BRANCH_NAME=$3
+WORK_DIR=$4
 
 # Set registry and clean branch name as global variables
 export REGISTRY=$REGISTRY  
 export CLEAN_BRANCH_NAME=$CLEAN_BRANCH_NAME 
 
+if [[ -n $WORK_DIR ]]
+then
+    echo "WORK_DIR ${WORK_DIR}"
+    cd $WORK_DIR
+fi  
 echo "Dev image check" 
 IMG_STR=`cat docker-compose.override.yml | grep 'devenv'| cut -d ":" -f 2-3`
 IMG_LIST=( $IMG_STR ) #convert string into an array
