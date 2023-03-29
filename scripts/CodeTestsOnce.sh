d73255612e495443420a4b556e4507ed32d83d41
Rahul <rahulpatidar0191@gmail.com>
2023-03-29 16:07:58 -0700

    add openapi

--- scripts/CodeTestsOnce.sh
+++ scripts/CodeTestsOnce.sh
@@ -7,9 +7,13 @@ then
 fi  
 
 echo "App health check"  # Check to see if the app container is running or not
-docker ps 
 sleep 5
 docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
+
+echo "Openapi link" # copy openapi files to samba mount 
+docker-compose exec -T webapp python -c "import requests; f=open('${APP_NAME}_${CLEAN_BRANCH_NAME}_openapi.json','w',encoding = 'utf-8'); f.write(requests.get('http://localhost:8000/openapi.json').text);f.close()"
+[ -d "mnt/samba/${APP_NAME}" ] || mkdir "mnt/samba/${APP_NAME}"
+docker cp "$(docker-compose ps -q webapp)":"/python/app/${APP_NAME}_${CLEAN_BRANCH_NAME}_openapi.json mnt/samba/${APP_NAME}_${CLEAN_BRANCH_NAME}_openapi.json"
 
 echo "Clean up old reports" 
 rm -f unittesting.xml coverage.xml typing.xml
