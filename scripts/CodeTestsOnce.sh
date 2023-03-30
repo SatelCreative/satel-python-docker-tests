c058a2196e7cafce4e556b8c7ad83a218ae9ca01
Rahul <rahulpatidar0191@gmail.com>
2023-03-29 17:28:05 -0700

    enable testing

--- scripts/CodeTestsOnce.sh
+++ scripts/CodeTestsOnce.sh
@@ -5,7 +5,7 @@ then
     echo "WORK_DIR ${WORK_DIR}"
     cd $WORK_DIR
 fi  
-echo ${CLEAN_BRANCH_NAME}
+
 echo "App health check"  # Check to see if the app container is running or not
 sleep 5
 docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
@@ -15,17 +15,17 @@ docker-compose exec -T webapp python -c "import requests; f=open('${CLEAN_BRANCH
 [ -d "/mnt/samba/${APP_NAME}" ] || mkdir -p "/mnt/samba/${APP_NAME}"
 docker cp "$(docker-compose ps -q webapp)":"/python/app/${CLEAN_BRANCH_NAME}_openapi.json" "/mnt/samba/${APP_NAME}/${CLEAN_BRANCH_NAME}_openapi.json"
 
-# echo "Clean up old reports" 
-# rm -f unittesting.xml coverage.xml typing.xml
+echo "Clean up old reports" 
+rm -f unittesting.xml coverage.xml typing.xml
 
-# echo "Code tests" 
-# ## Catch the exit codes so we don't exit the whole script before we are done.
-# ## Typing, linting, formatting check & unit and integration testing
-# docker-compose exec -T webapp validatecodeonce; STATUS1=$?
-# docker cp "$(docker-compose ps -q webapp)":/python/reports/typing.xml typing.xml
-# docker cp "$(docker-compose ps -q webapp)":/python/reports/unittesting.xml unittesting.xml
-# docker cp "$(docker-compose ps -q webapp)":/python/reports/coverage.xml coverage.xml
-# ## Return the status code
-# TOTAL=$((STATUS1))
+echo "Code tests" 
+## Catch the exit codes so we don't exit the whole script before we are done.
+## Typing, linting, formatting check & unit and integration testing
+docker-compose exec -T webapp validatecodeonce; STATUS1=$?
+docker cp "$(docker-compose ps -q webapp)":/python/reports/typing.xml typing.xml
+docker cp "$(docker-compose ps -q webapp)":/python/reports/unittesting.xml unittesting.xml
+docker cp "$(docker-compose ps -q webapp)":/python/reports/coverage.xml coverage.xml
+## Return the status code
+TOTAL=$((STATUS1))
 
 exit $TOTAL
