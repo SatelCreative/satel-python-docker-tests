e030b58a873ce0563b2f9f718615dd9b12fe9526
Rahul <rahulpatidar0191@gmail.com>
2023-03-29 17:14:50 -0700

    revert -

--- scripts/CodeTestsOnce.sh
+++ scripts/CodeTestsOnce.sh
@@ -11,9 +11,10 @@ sleep 5
 docker-compose exec -T webapp python -c "import requests; requests.get('http://localhost:8000/health')" || exit 1
 
 echo "Openapi link" # copy openapi files to samba mount 
-docker-compose exec -T webapp python -c "import requests; f=open('${APP_NAME}/${CLEAN_BRANCH_NAME}_openapi.json','w',encoding = 'utf-8'); f.write(requests.get('http://localhost:8000/openapi.json').text);f.close()"
+docker-compose exec -T webapp python -c "import requests; f=open('${APP_NAME}-${CLEAN_BRANCH_NAME}_openapi.json','w',encoding = 'utf-8'); f.write(requests.get('http://localhost:8000/openapi.json').text);f.close()"
 [ -d "mnt/samba/${APP_NAME}" ] || mkdir -p "mnt/samba/${APP_NAME}"
-docker cp "$(docker-compose ps -q webapp)":"/python/app/${APP_NAME}/${CLEAN_BRANCH_NAME}_openapi.json" "mnt/samba/${APP_NAME}/${CLEAN_BRANCH_NAME}_openapi.json"
+ls "/python/app/${APP_NAME}"
+docker cp "$(docker-compose ps -q webapp)":"/python/app/${APP_NAME}-${CLEAN_BRANCH_NAME}_openapi.json" "mnt/samba/${APP_NAME}-${CLEAN_BRANCH_NAME}_openapi.json"
 
 echo "Clean up old reports" 
 rm -f unittesting.xml coverage.xml typing.xml
