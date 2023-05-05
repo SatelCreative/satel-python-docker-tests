33addf4e131f24eeb7dd5b79b295862135b1b945
rahul <rahulpatidar0191@gmail.com>
2023-05-05 08:08:19 -0700

    fix syntax

--- scripts/CodeTestsOnce.sh
+++ scripts/CodeTestsOnce.sh
@@ -21,17 +21,15 @@ rm -f unittesting.xml coverage.xml typing.xml typing-server.xml typing-integrati
 echo "Code tests" 
 ## Catch the exit codes so we don't exit the whole script before we are done.
 ## Typing, linting, formatting check & unit and integration testing
-if [[ $CONTAINER_NAME == "pim_api"]]
-then
-    docker-compose exec -T ${CONTAINER_NAME} flake8; STATUS1=$?
-fi   
+ 
 
-if [[ $CONTAINER_NAME == "webapp" ]] 
-then
+if [[ $CONTAINER_NAME == "webapp" ]]; then
     docker-compose exec -T ${CONTAINER_NAME} validatecodeonce; STATUS1=$?
     docker cp "$(docker-compose ps -q ${CONTAINER_NAME})":/python/reports/typing.xml typing.xml
     docker cp "$(docker-compose ps -q ${CONTAINER_NAME})":/python/reports/unittesting.xml unittesting.xml
     docker cp "$(docker-compose ps -q ${CONTAINER_NAME})":/python/reports/coverage.xml coverage.xml
+else
+   docker-compose exec -T ${CONTAINER_NAME} flake8; STATUS1=$?    
 fi
 
 ## Return the status code
